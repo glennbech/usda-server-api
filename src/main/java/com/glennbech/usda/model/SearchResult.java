c6533c8c95892f0ffaa19d07fe42a6a6619b7786
glennbech <glenn.bech@inmeta.com>
2013-07-15 11:57:15 +0200

    paging

---
+++ src/main/java/com/glennbech/usda/model/SearchResult.java
@@ -0,0 +1,11 @@
+package com.glennbech.usda.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: glennbech
+ * Date: 7/15/13
+ * Time: 11:13 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SearchResult {
+}
