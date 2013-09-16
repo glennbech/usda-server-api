87460e86cf97687b1b5756c50ace76da110c83ac
glennbech <glenn.bech@inmeta.com>
2013-09-16 02:37:33 +0200

    fix

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web:    ja  va $JAVA_OPTS -jar target/dependency/jetty-runner.jar --port $PORT target/*.war
+web:    java $JAVA_OPTS -jar target/dependency/jetty-runner.jar --port $PORT target/*.war
--- src/main/java/com/glennbech/usda/RelatedFoodsInitServlet.java
+++ src/main/java/com/glennbech/usda/RelatedFoodsInitServlet.java
@@ -1,8 +1,11 @@
 package com.glennbech.usda;
 
+import com.glennbech.usda.model.SimilarFoods;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import java.io.IOException;
 import java.io.InputStream;
 
 /**
@@ -13,11 +16,16 @@ public class RelatedFoodsInitServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
+
         InputStream is = config.getServletContext().getResourceAsStream("/WEB-INF/distances.csv");
         if (is == null ) {
             throw new IllegalArgumentException();
         }
-        long now = System.currentTimeMillis();
-//            SimilarFoods.initialize(is);
+        try {
+            long now = System.currentTimeMillis();
+            SimilarFoods.initialize(is);
+        } catch (IOException e) {
+            throw new ServletException(e);
+        }
     }
 }
